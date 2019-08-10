0e153c2fe15ae9341df60ece5b41739719465b3d
Petra Rábai <petra.rabai@icloud.com>
2019-08-10 13:22:18 +0200

    change design

---
+++ js/actions.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  var acc = document.getElementsByClassName("accordion");
+  var panel = document.getElementsByClassName("panel");
+
+  for (var i = 0; i < acc.length; i++) {
+    acc[i].onclick = function() {
+      var setClasses = !this.classList.contains("active");
+      setClass(acc, "active", "remove");
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+
+      setClass(panel, "show", "remove");
+
+      if (setClasses) {
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+      }
+    };
+  }
+
+  function setClass(els, className, fnName) {
+    for (var i = 0; i < els.length; i++) {
+      els[i].classList[fnName](className);
+    }
+  }
+});
