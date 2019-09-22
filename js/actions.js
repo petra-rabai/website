012ddf5a9e1299a486c859a147d2a7dfdb3e477d
Petra <petra.rabai@icloud.com>
2019-09-22 13:20:04 +0200

    modify carousel handler function

--- js/actions.js
+++ js/actions.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   var acc = document.getElementsByClassName("accordion");
   var panel = document.getElementsByClassName("panel");
-
+  var imageshow =document.getElementById("imageone");
   for (var i = 0; i < acc.length; i++) {
     acc[i].onclick = function() {
       var setClasses = !this.classList.contains("active");
@@ -10,8 +10,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
       var panel = this.nextElementSibling;
       if (panel.style.display === "block") {
         panel.style.display = "none";
+        imageshow.style.display = "none"
       } else {
         panel.style.display = "block";
+        imageshow.style.display = "block";
       }
 
       setClass(panel, "show", "remove");
@@ -48,12 +50,16 @@ function showSlidesdot(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
+  var imageshow =document.getElementById("imageone");
   if (n > slides.length) {
     slideIndex = 1;
   }
   if (n < 1) {
     slideIndex = slides.length;
   }
+  
+  imageshow.style.display = "none";
+  
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
